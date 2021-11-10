890a27e77ae72ef6e4c8228494b2b83ae904ac6f
mbueno <buenoperez.miguelangel@gmail.com>
2021-11-10 17:42:22 -0500

    Formulario registro inicial

--- app/src/main/java/com/appvoto/tictaxi/Home.java
+++ app/src/main/java/com/appvoto/tictaxi/Home.java
@@ -2,7 +2,11 @@ package com.appvoto.tictaxi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import Intro.LosPermisos;
 
 public class Home extends AppCompatActivity {
 
@@ -10,5 +14,9 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void irRegistro(View view) {
+        startActivity(new Intent(Home.this, Registro.class));
     }
 }
---
+++ app/src/main/java/com/appvoto/tictaxi/Registro.java
@@ -0,0 +1,14 @@
+package com.appvoto.tictaxi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Registro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registro);
+    }
+}
