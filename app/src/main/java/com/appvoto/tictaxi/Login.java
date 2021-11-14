26cb641c3b5180b3734ce6fd8a625753ea76ed49
mbueno <buenoperez.miguelangel@gmail.com>
2021-11-13 22:25:54 -0500

    Cargando facebook con firebase

--- app/src/main/java/com/appvoto/tictaxi/Login.java
+++ app/src/main/java/com/appvoto/tictaxi/Login.java
@@ -8,6 +8,7 @@ import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
@@ -22,9 +23,15 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.appvoto.tictaxi.Util.SharedPreferencesUtils;
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
 
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -38,6 +45,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.GoogleAuthProvider;
 
@@ -50,11 +58,15 @@ import java.util.Objects;
 public class Login extends AppCompatActivity{
     TextInputEditText correo, passw;
     TextView olvido, registrar;
-    ImageView btn_google, btn_facebook;
+    ImageView btn_google;
+    LoginButton btn_facebook;
     Button ingresar;
     FirebaseAuth auth;
     ProgressDialog dialogo;
     GoogleSignInClient mGoogleSignInClient;
+    private static final String TAG = "FACEBOOK";
+    private static final int RC_SIGN_IN = 12345;
+    CallbackManager mCallbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -143,8 +155,52 @@ public class Login extends AppCompatActivity{
             }
         });
 
+        //region Loguearse con Facebook
+        mCallbackManager = CallbackManager.Factory.create();
+        btn_facebook.setReadPermissions("email", "public_profile");
+        btn_facebook.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                handleFacebookAccessToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(Login.this, "Login Cancelado", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+        //endregion
+
     }
 
+    //region Loguearse con Facebook...
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void handleFacebookAccessToken(AccessToken accessToken) {
+        AuthCredential credential = FacebookAuthProvider.getCredential(accessToken.getToken());
+        auth.signInWithCredential(credential)
+                .addOnCompleteListener(Login.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(Login.this, "Login accedido", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(Login.this, Home.class));
+                        } else {
+                            Toast.makeText(Login.this, "Login fallado", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+    //endregion
 
     //region Loguearse con Google...
 
